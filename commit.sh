103de5ad2e17dba1c9354e2b528cef047098ce7f
Erik Ylvisaker <erik@vermiliontower.com>
2018-03-23 20:05:10 -0700

    Adding configuration from Vader on Fri Mar 23 20:05:10 DST 2018.

--- commit.sh
+++ commit.sh
@@ -7,6 +7,9 @@ cp ~/.bash_profile bash_profile
 cp ~/.vimrc vimrc
 
 git add bash_profile vimrc
+git add commit.sh install.sh pull.sh
 
 git commit -m "Adding configuration from `hostname` on `date`."
+
+git status
 
