030cfa20d4392f6f6333d218b07830b9ea5c911d
Erik Ylvisaker <eylvisaker@impinj.com>
2018-05-29 10:28:13 -0700

    Adding configuration from vagrant-eylvisaker-jfsxg on Tue May 29 10:28:13 PDT 2018.

--- commit.sh
+++ commit.sh
@@ -11,7 +11,14 @@ git add commit.sh install.sh pull.sh uninstall.sh
 
 message=$1
 if [[ -z $1 ]]; then
-  $message="Adding configuration from `hostname` on `date`."
+  message="Adding configuration from `hostname` on `date`."
+fi
+
+staged=`git diff --name-only --cached 2> /dev/null | wc -l`
+
+if [[ $staged -le 0 ]]; then
+  echo "No changes to commit."
+  exit 1
 fi
 
 git commit -m "$message"
